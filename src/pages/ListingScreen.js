21acafb7e9cdad40e912adc8d24a7fd628c1dc45
Sezer Turkdal <sezer.turkdal@hotmail.com>
2022-12-04 03:08:31 +0300

    AsyncStorage added

--- src/pages/ListingScreen.js
+++ src/pages/ListingScreen.js
@@ -3,20 +3,43 @@ import { View, Text, TouchableOpacity, Button, StyleSheet, FlatList, ScrollView,
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faAdd } from '@fortawesome/free-solid-svg-icons/faAdd'
 import CustomRow from '../components/CustomRow';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function ListingScreen ({navigation}) {
 
   const [itemList, setitemList] = useState(0);
 
   useEffect(() => {
-   getData();
+    readData();
   });
+
+  const readData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('SimpsonsList');
+      if (value !== null) {
+        setitemList(JSON.parse(value));
+      }else{
+        getData()
+      }
+    } catch (e) {
+      console.log('Error when fetching data from storage')
+    }
+  };
+
+  const saveData = async (json) => {
+    try {
+      await AsyncStorage.setItem('SimpsonsList', JSON.stringify(json))
+    } catch (e) {
+      console.log('Error when saving data', e)
+    }
+  }
 
   const getData = () => {
     return fetch('https://5fc9346b2af77700165ae514.mockapi.io/simpsons')
       .then((response) => response.json())
       .then((json) => {
         setitemList(json)
+        saveData(json)
         return json;
       })
       .catch((error) => {
@@ -33,7 +56,7 @@ function ListingScreen ({navigation}) {
           <FlatList
             data={itemList}
             style={styles.list}
-            keyExtractor={(itm) => itm.id.toString()}   
+            keyExtractor={(itm) => itm.id}   
             renderItem={({ item, index }) => <CustomRow
                 id={index+1}
                 name={item.name}
